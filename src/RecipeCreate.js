d324e69e544647fcfaca79448fdcdf3d25532d18
Gail Ang <145751276+MaryGailAng@users.noreply.github.com>
2024-06-21 23:43:38 +0900

    Add files via upload

---
+++ src/RecipeCreate.js
@@ -0,0 +1,128 @@
+import React, { useState } from "react";
+
+function RecipeCreate({createRecipe}) {
+
+  // TODO: When the form is submitted, a new recipe should be created, and the form contents cleared.
+  // TODO: Add the required input and textarea form elements.
+  // TODO: Add the required submit and change handlers.
+ 
+
+  const initialFormState= {
+    name: "Name",
+    cuisine: "Cuisine",
+    photo: "URL",
+    ingredients: "Ingredients",
+    preparation: "Preparation",
+  };
+
+  const [formData, setFormData] = useState({...initialFormState});
+  
+  const handleChange = ({ target }) => {
+    setFormData({
+      ...formData,
+      [target.name]: target.value,
+    });
+  };
+  
+  const handleSubmit =(event) => {
+    event.preventDefault();
+    console.log(formData);
+    
+    const newRecipe = (formData);
+    createRecipe(newRecipe);
+
+    setFormData({...initialFormState});
+  };
+
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+              <th for="name">Name</th>
+              <th for="cuisine">Cuisine</th>
+              <th for="photo">Photo</th>
+              <th for="ingredients">Ingredients</th>
+              <th for="preparation">Preparation</th>
+              <th for="actions">Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td for="name">
+              <form name="create">
+                <input
+                  id="name"
+                  type="text"
+                  name="name"
+                  onChange={handleChange}
+                  value={formData.name}
+                  //onChange={handleNameChange}
+                  //value={name}
+                />
+              </form>  
+            </td>
+            <td for="cuisine">
+              <form name="create">  
+                <input
+                  id="cuisine"
+                  type="text"
+                  name="cuisine"
+                  onChange={handleChange}
+                  value={formData.cuisine}
+                  //onChange={handleCuisineChange}
+                  //value={cuisine}
+                />
+              </form>  
+            </td>
+            <td for="photo">
+              <form name="create">  
+                <input
+                  id="photo"
+                  type="url"
+                  name="photo"onChange={handleChange}
+                  value={formData.photo}
+                  //onChange={handlePhotoChange}
+                  //value={photo}
+                />
+              </form>
+            </td>
+            <td for="ingredients">
+              <form name="create">  
+                <textarea
+                  id="ingredients"
+                  type="text"
+                  name="ingredients"
+                  onChange={handleChange}
+                  value={formData.ingredients}
+                  //onChange={handleIngredientsChange}
+                  //value={ingredients}
+                />
+              </form>
+            </td>
+            <td for="preparation">
+              <form name="create">
+                <textarea
+                  id="preparation"
+                  type="text"
+                  name="preparation"
+                  onChange={handleChange}
+                  value={formData.preparation}
+                  //onChange={handlePreparationChange}
+                  //value={preparation}
+                />
+              </form>
+            </td>
+            <td>
+              <form onSubmit={handleSubmit} name="create">
+                <button type="submit" >Create</button>
+              </form>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+  </div>  
+  );
+}
+
+export default RecipeCreate;
